03fc9663b912fea80bd4cd063154bd66781d246a
Monty <tecnisoft01@gmail.com>
2023-03-11 23:51:17 -0300

    Host_Ready

--- script.sh
+++ script.sh
@@ -2,5 +2,9 @@
 
 sudo yum update
 sudo yum install -y vim curl telnet unzip wget net-tools htop nmap ansible epel-release nginx nano
-sudo su
-service nginx restart
+sudo service nginx restart
+#Criar usuário
+sudo adduser -m -s /bin/bash monty
+
+# Senha do usuário
+echo "monty:senha123" | sudo chpasswd
