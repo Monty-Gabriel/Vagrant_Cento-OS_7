3d6c976585dc70d3e438d39c53cadbdc009f8897
Monty <tecnisoft01@gmail.com>
2023-03-26 18:56:21 -0300

    Install Python Pack

--- script.sh
+++ script.sh
@@ -2,9 +2,15 @@
 
 sudo yum update
 sudo yum install -y vim curl telnet unzip wget net-tools htop nmap ansible epel-release nginx nano
-sudo service nginx restart
+sudo systemctl restart nginx.service
 #Criar usuário
 sudo adduser -m -s /bin/bash monty
 
 # Senha do usuário
 echo "monty:senha123" | sudo chpasswd
+
+# Reboot
+sudo reboot now
+
+# Senha root
+echo "root:senha123" | sudo chpasswd
